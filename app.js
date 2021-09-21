0feca0b407b61a959c59f4144b08fc103bebf114
Christian Vilen <c.vilen@outlook.com>
2021-09-21 22:18:17 +0300

    added client for backend

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require('express')
 const app = express()
+const port = 8000
+const calculate = require("./src/calculate");
 
+app.post('/', async (req, res) => {
+  const result = await calculate(req)
+  res.json(result)
+})
+
+app.listen(port, () => console.log(`Running on ${port}`))
--- src/calculate.js
+++ src/calculate.js
@@ -1,4 +1,4 @@
-function calculate(expression) {
+const calculate = (expression) => {
   if (!expression.operator) {
     return console.error("missing operator!")
   }
@@ -26,10 +26,4 @@ const subtract = (input, input2) => input - input2
 const multiply = (input, input2) => input * input2
 const divide = (input, input2) => input / input2
 
-console.log("1", calculate({left: {left: 5, right: 4, operator: "*"}, right: 4, operator: "+"}))
-console.log("2", calculate({
-  left: {left: 5, right: 4, operator: "*"},
-  right: {left: 5, right: 4, operator: "-"},
-  operator: "+"
-}))
-console.log("3", calculate({left: 12, right: 4, operator: "/"}))
+module.exports = calculate
